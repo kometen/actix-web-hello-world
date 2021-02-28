8fdff0faa4bd494fc4715636f85deea4d7d29561
Claus Guttesen <kometen@gmail.com>
2021-02-28 21:47:58 +0100

    Added json serialize and deserialize.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct Info {
     user_id: u32,
-    friend: String,
+    username: String,
 }
 
 #[get("/")]
@@ -12,16 +12,21 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hej med dig!")
 }
 
-#[get("/users/{user_id}/{friend}")]
-async fn user(info: web::Path<Info>) -> Result<String> {
-    Ok(format!(
-        "Hej {}, bruger-id {}!",
-        info.friend, info.user_id))
+#[get("/users/{user_id}/{username}")]
+async fn user(info: web::Path<Info>) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json(Info {
+        user_id: info.user_id, username: info.username.to_string(),
+    }))
 }
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body.to_lowercase())
+}
+
+#[post("/identify")]
+async fn identify(info: web::Json<Info>) -> Result<String> {
+    Ok(format!("Welcome {}, your id is {}", info.username, info.user_id))
 }
 
 async fn manual_hello() -> impl Responder {
@@ -37,6 +42,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .service(user)
+            .service(identify)
             .route("/hey", web::get().to(manual_hello))
     })
         .bind("127.0.0.1:8080")?
